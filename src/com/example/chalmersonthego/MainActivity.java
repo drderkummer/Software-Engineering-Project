971044ac756164da0c693e8817a6a8929f1a7b90
Anders Nordin <anders.t.nordin@gmail.com>
2013-04-16 00:16:12 +0200

    Basic Map Shell

--- src/com/example/chalmersonthego/MainActivity.java
+++ src/com/example/chalmersonthego/MainActivity.java
@@ -1,23 +1,23 @@
 package com.example.chalmersonthego;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends MapActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+	    
+	    MapView mapView = (MapView) findViewById(R.id.mapview);
+	    mapView.setBuiltInZoomControls(true);
+	}
     
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
 }
