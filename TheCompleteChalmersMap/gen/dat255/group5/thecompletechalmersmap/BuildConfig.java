2279e690c287e030b99a9a6c45f2727af836b6d5
Fredrik Einarsson <freein@student.chalmers.se>
2013-04-15 14:00:35 +0200

    Try again

--- TheCompleteChalmersMap/gen/dat255/group5/thecompletechalmersmap/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package dat255.group5.thecompletechalmersmap;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
