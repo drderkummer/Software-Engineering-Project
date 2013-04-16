5ea39935c1ef410ad8d0aabdff5db3fb4f501f98
Fredrik <freein@student.chalmers.se>
2013-04-16 20:17:28 +0200

    Added starting point and zoom level

--- src/com/example/chalmersonthego/MainActivity.java
+++ src/com/example/chalmersonthego/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.chalmersonthego;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 import group5.database.DAO;
 import android.app.Activity;
 import android.os.Bundle;
@@ -7,11 +10,15 @@ import android.os.Bundle;
 public class MainActivity extends Activity {
 	
 	private DAO dao;
+	private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //Get the instance of GoogleMap
+        setUpMapIfNeeded();
         
         //Open connection to the Database
         dao = new DAO(this);
@@ -23,5 +30,20 @@ public class MainActivity extends Activity {
     	
     	//Close connection to the databse
     	dao.close();
+    	
+    	super.onDestroy();
+    }
+    
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (map == null) {
+            map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                                .getMap();
+            // Check if we were successful in obtaining the map.
+            if (map != null) {
+                // The Map is verified. It is now safe to manipulate the map.
+
+            }
+        }
     }
 }
