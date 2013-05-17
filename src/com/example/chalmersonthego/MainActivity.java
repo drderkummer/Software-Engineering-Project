70fb9fb0299477616cb121f8b144d478b7fa60b2
Niklas <niklas.johansson.91@gmail.com>
2013-05-17 14:28:51 +0200

    Added a better step tracker, that actually works

--- src/com/example/chalmersonthego/MainActivity.java
+++ src/com/example/chalmersonthego/MainActivity.java
@@ -44,15 +44,24 @@ import com.google.android.gms.maps.model.Marker;
 @SuppressLint("NewApi")
 public class MainActivity extends Activity implements SensorEventListener {
 
+	// Treadmill stuff
+	private int steps = 0;
+	private float mLimit = 10;
+	private float mLastValues[] = new float[3 * 2];
+	private float mScale[] = new float[2];
+	private float mYOffset;
+
+	private float mLastDirections[] = new float[3 * 2];
+	private float mLastExtremes[][] = { new float[3 * 2], new float[3 * 2] };
+	private float mLastDiff[] = new float[3 * 2];
+	private int mLastMatch = -1;
+	SensorManager mSensorManager;
+	private Sensor mSensor;
+	// End of treadmill
+
 	private DAO dao;
 	private CustomGoogleMaps customMaps;
 	private NavigationManager navigationManager;
-
-	// Treadmill
-	private SensorManager mSensorManager;
-	private Sensor mSensor;
-	private int steps = 0;
-	// End of treadmill
 
 	// Keep track of daymode and nightmode
 	Boolean nightModeOn = false;
@@ -67,6 +76,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
@@ -83,7 +93,6 @@ public class MainActivity extends Activity implements SensorEventListener {
 		dao = new DAO(this);
 		dao.open();
 		insertDataForTheFirstTime();
-
 	}
 
 	/**
@@ -252,6 +261,19 @@ public class MainActivity extends Activity implements SensorEventListener {
 		}
 	}
 
+	private void startTreadmill() {
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mSensorManager.registerListener(this, mSensor,
+				SensorManager.SENSOR_DELAY_GAME);
+
+		int h = 480;
+		mYOffset = h * 0.5f;
+		mScale[0] = -(h * 0.5f * (1.0f / (SensorManager.STANDARD_GRAVITY * 2)));
+		mScale[1] = -(h * 0.5f * (1.0f / (SensorManager.MAGNETIC_FIELD_EARTH_MAX)));
+
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the options menu from XML
@@ -363,28 +385,6 @@ public class MainActivity extends Activity implements SensorEventListener {
 		}
 	}
 
-	public void startTreadmill() {
-		// Setup the accelerometer
-		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		mSensorManager.registerListener(this, mSensor,
-				SensorManager.SENSOR_DELAY_GAME);
-	}
-
-	/**
-	 * Treadmill
-	 */
-	public void onSensorChanged(SensorEvent event) {
-
-		if (event.values[1] > 10) {
-			Toast.makeText(this, "You have taken " + steps + " steps!",
-					Toast.LENGTH_SHORT).show();
-			steps++;
-
-		}
-
-	}
-
 	/**
 	 * @see android.app.Activity#onCreateDialog(int)
 	 * @param id
@@ -423,6 +423,59 @@ public class MainActivity extends Activity implements SensorEventListener {
 				customMaps.removeAllMarkerFromMap();
 				showRooms();
 				break;
+			}
+		}
+	}
+
+	/**
+	 * Treadmill stuff
+	 * 
+	 * @param event
+	 */
+	public void onSensorChanged(SensorEvent event) {
+		Sensor sensor = event.sensor;
+		synchronized (this) {
+			int j = (sensor.getType() == Sensor.TYPE_ACCELEROMETER) ? 1 : 0;
+			if (j == 1) {
+				float vSum = 0;
+				for (int i = 0; i < 3; i++) {
+					final float v = mYOffset + event.values[i] * mScale[j];
+					vSum += v;
+				}
+				int k = 0;
+				float v = vSum / 3;
+
+				float direction = (v > mLastValues[k] ? 1
+						: (v < mLastValues[k] ? -1 : 0));
+				if (direction == -mLastDirections[k]) {
+					// Direction changed
+					int extType = (direction > 0 ? 0 : 1); // minumum or
+															// maximum?
+					mLastExtremes[extType][k] = mLastValues[k];
+					float diff = Math.abs(mLastExtremes[extType][k]
+							- mLastExtremes[1 - extType][k]);
+
+					if (diff > mLimit) {
+
+						boolean isAlmostAsLargeAsPrevious = diff > (mLastDiff[k] * 2 / 3);
+						boolean isPreviousLargeEnough = mLastDiff[k] > (diff / 3);
+						boolean isNotContra = (mLastMatch != 1 - extType);
+
+						if (isAlmostAsLargeAsPrevious && isPreviousLargeEnough
+								&& isNotContra) {
+							// Step confirmed!
+							Log.i("main", "step");
+							steps++;
+							getActionBar().setTitle("You have taken " + steps + " steps.");
+							mLastMatch = extType;
+						} else {
+							mLastMatch = -1;
+						}
+					}
+					mLastDiff[k] = diff;
+				}
+				mLastDirections[k] = direction;
+				mLastValues[k] = v;
 			}
 		}
 	}
