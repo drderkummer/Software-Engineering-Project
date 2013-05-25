4023e166023f8ec16bfb8b2fa1219ca0ff293f93
Anders Nordin <anders.t.nordin@gmail.com>
2013-05-25 18:04:55 +0200

    small fix

--- src/dat255/group5/chalmersonthego/CustomGoogleMaps.java
+++ src/dat255/group5/chalmersonthego/CustomGoogleMaps.java
@@ -222,7 +222,7 @@ public class CustomGoogleMaps {
 
 				LatLng _latlng = latlng;
 				
-				if(!arrivedFrom.equals("Target Position") && !arrivedFrom.equals("My Position"))
+				if(!wantToGo.equals("Target Position") && !wantToGo.equals("My Position"))
 					_latlng = dao.getRoomCoordinates(start);			
 
 
